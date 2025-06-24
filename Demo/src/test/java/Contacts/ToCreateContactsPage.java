ef24217c56713ac427fd67801e2e7a8963652219
Admin <Admin@DESKTOP-GUADMG8>
2025-06-24 13:21:33 +0530

    First Project

---
+++ Demo/src/test/java/Contacts/ToCreateContactsPage.java
@@ -0,0 +1,9 @@
+package Contacts;
+
+public class ToCreateContactsPage {
+
+	public static void main(String[] args) {
+		System.out.println("Contacts page is created");
+	}
+
+}
---
+++ Demo/src/test/java/organization/ToCreateOrganizationPage.java
@@ -0,0 +1,9 @@
+package organization;
+
+public class ToCreateOrganizationPage {
+
+	public static void main(String[] args) {
+		System.out.println("Organization page is created sucessfully");
+	}
+
+}
